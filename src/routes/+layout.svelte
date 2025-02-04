235af26d09a29f43a109695d79aa85a3c17404f2
Erisa Puka <EriPuk19@htl-shkoder.com>
2025-02-04 12:14:16 +0100

    message

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css"
+    let {children}= $props();
+</script>
+
+<header> HEADER </header>
+<main> {@render children()} </main>
+<footer> FOOTER </footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    let message = "meow meow";
+</script>
+ 
+<div> 
+    <h1>{message}</h1>
+    <p>Write whatever you want:</p>
+    <input type="text" bind:value={message}/>
+</div>
+<style>
+input[type="text"] {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 10px;
+    font-size: 16px;
+    border: 2px solid #ddd;
+    border-radius: 4px;
+    width: 300px;
+    margin-top: 10px;
+}
+
+h1 {
+    font-size: 36px;
+    color: #333;
+    margin-bottom: 20px;
+}
+
+p {
+    font-size: 18px;
+    color: #555;
+}
+</style>
